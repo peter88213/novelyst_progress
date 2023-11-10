398503354340dc1f87009cfff9160317aa73e948
peter88213 <peter.88213@t-online.de>
2023-11-10 21:06:19 +0100

    Change build path to novxlib-Alpha

--- tools/build_novelyst_progress.py
+++ tools/build_novelyst_progress.py
@@ -12,7 +12,7 @@ License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 """
 import os
 import sys
-sys.path.insert(0, f'{os.getcwd()}/../../novxlib/src')
+sys.path.insert(0, f'{os.getcwd()}/../../novxlib-Alpha/src')
 import inliner
 
 SRC = '../src/'
@@ -24,7 +24,7 @@ TARGET_FILE = f'{BUILD}novelyst_progress.py'
 def main():
     inliner.run(SOURCE_FILE, TARGET_FILE, 'nvprogresslib', '../../novelyst_progress/src/')
     inliner.run(TARGET_FILE, TARGET_FILE, 'novelystlib', '../../novelyst/src/')
-    inliner.run(TARGET_FILE, TARGET_FILE, 'novxlib-Alpha', '../../novxlib/src/')
+    inliner.run(TARGET_FILE, TARGET_FILE, 'novxlib-Alpha', '../../novxlib-Alpha/src/')
     print('Done.')
 
 
